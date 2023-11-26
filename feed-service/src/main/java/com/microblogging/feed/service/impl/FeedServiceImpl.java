9eb9a069db7f631154e0aee3029389efa0bdd01f
moukthika-vuyyuru <moukthikareddy.vuyyuru@sjsu.edu>
2023-11-25 19:25:37 -0800

    Refactored code

--- feed-service/src/main/java/com/microblogging/feed/service/impl/FeedServiceImpl.java
+++ feed-service/src/main/java/com/microblogging/feed/service/impl/FeedServiceImpl.java
@@ -3,6 +3,7 @@ package com.microblogging.feed.service.impl;
 import com.microblogging.feed.model.UserTimeline;
 import com.microblogging.feed.repository.UserTimelineRepository;
 import com.microblogging.feed.service.FeedService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Slf4j
 public class FeedServiceImpl implements FeedService {
 
     private final UserTimelineRepository userTimelineRepository;
@@ -22,8 +24,10 @@ public class FeedServiceImpl implements FeedService {
     @Override
     public List<UserTimeline> getUserTimeline(String userId, int limit, LocalDateTime olderThan) {
         if (olderThan == null) {
+            log.info("Retrieving feed for user {} with limit {}", userId, limit);
             return userTimelineRepository.findByUserIdWithLimit(userId, limit);
         } else {
+            log.info("Retrieving feed for user {} older than {}", userId, olderThan);
             return userTimelineRepository.findByUserIdAndOlderThanWithLimit(userId, olderThan, limit);
         }
     }
